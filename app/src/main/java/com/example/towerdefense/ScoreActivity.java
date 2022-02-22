51140649a771d2f40f87bf8d2b3f22ae3ab53738
benjamin <benjamincasse09@gmail.com>
2022-02-22 18:53:14 +0100

    boutton de partage dans l'activity addScore

--- app/src/main/java/com/example/towerdefense/ScoreActivity.java
+++ app/src/main/java/com/example/towerdefense/ScoreActivity.java
@@ -4,21 +4,46 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class ScoreActivity extends AppCompatActivity {
 
     Score score;
     TextView scoreText;
     SharedPreferences sharedPreferences;
+    Button shareButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
+        shareButton = (Button) findViewById(R.id.shareButton);
+        shareButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String input = scoreText.getText().toString();
+                String[] lines = input.split( "\n" );
+                String bestScore = lines[0].split( "1." )[1];;
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                String sharedSubject = "Meilleur score TowerDefense";
+                String sharedBody = "Mon meilleur score est de " + bestScore + " essaye de le battre !";
+                intent.putExtra(Intent.EXTRA_SUBJECT,sharedSubject);
+                intent.putExtra(Intent.EXTRA_TEXT, sharedBody);
+                startActivity(Intent.createChooser(intent, "Partager votre meilleur score"));
+            }
+        });
 
         this.scoreText = (TextView) findViewById(R.id.scorePrint);
         sharedPreferences = getSharedPreferences("TowerDefense", MODE_PRIVATE);
