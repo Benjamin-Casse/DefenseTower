728f02248b26125c10c59a7c3b4a1acf8ae60134
gabe <gabriel.chastaing@gmail.com>
2022-02-22 18:55:49 +0100

    ajout truc game activity

--- app/src/main/java/com/example/towerdefense/GameActivity.java
+++ app/src/main/java/com/example/towerdefense/GameActivity.java
@@ -10,12 +10,15 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class GameActivity extends AppCompatActivity {
 
     TextView nbVies;
     TextView nbSec;
     TextView nbKillAvantUlti;
     TextView debugPrint;
+    TextView textUlti;
 
     Grid oui = new Grid();
     Handler handler = new Handler();
@@ -57,18 +60,23 @@ public class GameActivity extends AppCompatActivity {
         nbKillAvantUlti = (TextView) findViewById(R.id.nbAvantUlti);
         nbVies = (TextView) findViewById(R.id.nbVies);
         nbSec = (TextView) findViewById(R.id.score);
+        textUlti = (TextView) findViewById(R.id.textUlti);
+        textUlti.setVisibility(View.INVISIBLE);
 
         //lance le debut de la partie
         handler.post(update);
     }
 
-
-
     public void updateGameVariable(){
         debugPrint.setText(oui.displayGrid());
-        nbSec.setText("" + nbSecs);
+        nbSec.setText(nbSecs + "s");
         nbKillAvantUlti.setText("" + oui.getNbEnnemieAvantUlt());
         nbVies.setText("" + oui.getNbVie());
+        if(oui.getNbEnnemieAvantUlt() <= 0){
+            textUlti.setVisibility(View.VISIBLE);
+        } else {
+            textUlti.setVisibility(View.INVISIBLE);
+        }
     }
 
     public void endOfGame(){
@@ -80,16 +88,12 @@ public class GameActivity extends AppCompatActivity {
         if(!sharedPreferences.contains("score")){
             score.addScore(nbSecs);
             editor.putString("score", score.getScoreTab());
-
         } else {
             score.setScoreTab(sharedPreferences.getString("score",""));
             score.addScore(nbSecs);
             editor.putString("score", score.getScoreTab());
         }
         editor.apply();
-        Log.d("ouioui", score.toString());
-
-        Log.d("oui", sharedPreferences.getString("score", ""));
 
         Intent intent = new Intent(this, ScoreActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
